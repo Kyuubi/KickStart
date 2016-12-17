9671de6b86f79ca99eb660609dfdd9bf9e20afc8
Kyuubi <parneh@gmail.com>
2016-12-17 15:43:38 +0530

    First small experiment with baseic data summary

---
+++ Category_Seperation.py
@@ -0,0 +1,48 @@
+import support
+import glob
+import ijson
+import json
+
+Cat_dict = {}
+
+def json_load(filename):
+	with open(filename, 'r') as fp:
+		parser = ijson.parse(fp)
+		flag_set=0
+		# for i in parser:
+		# 	print i
+		pledge = -1
+		for prefix, event, value in parser:
+			if prefix.endswith('.pledged'):
+				if pledge == -1:
+					pledge = float(value)
+				else:
+					print "Last value still not flushed out..."
+
+			if prefix.endswith('.category.name'):
+				# flag_set=0
+				if value not in Cat_dict.keys():
+					Cat_dict[value] = {}
+					Cat_dict[value]["Count"] = 1
+					Cat_dict[value]["TotalPledges"] = pledge
+					Cat_dict[value]["AveragePledge"] = pledge
+					pledge = -1
+
+					# print "Category Type = ",value
+				else:
+					Cat_dict[value]["Count"] = Cat_dict[value]["Count"]+1
+					Cat_dict[value]["TotalPledges"] = Cat_dict[value]["TotalPledges"]+pledge
+					Cat_dict[value]["AveragePledge"] = (Cat_dict[value]["AveragePledge"]*(Cat_dict[value]["Count"]-1) + pledge)/float(Cat_dict[value]["Count"])
+					pledge = -1
+
+
+	for key,value in Cat_dict.iteritems():
+		print key," = ", value
+
+
+DatasetPath = support.get_config("GlobalSettings", "datasetloc")
+files = glob.glob(DatasetPath+'*.json')
+for i in files:
+	json_load(i)
+
+support.write_json_to_file('Category_Summary.json', Cat_dict)
--- support.py
+++ support.py
@@ -13,6 +13,20 @@ def write_json_to_file(filepath, data_dict):
     with open(filepath, 'w') as fp:
         json.dump(data_dict, fp, indent=4)
 
+
+def get_environ(key):
+    if key in os.environ:
+        return os.environ[key]
+    else:
+        print 'environment variable %s not found' % key;
+        sys.exit(1)
+
+def get_config(section, option):
+
+    configPath = get_environ('ND_CONFIG_PATH');
+    val = get_config_from_path(configPath, section)
+    return val[option]
+
 def get_config_from_path(configPath, section):    
     read = False
     result = "none"
@@ -28,6 +42,6 @@ def get_config_from_path(configPath, section):
                 result = dict(params)
                 read = True
         except: #read failed due currupted ini file that can happen due to suddent power of during update            
-            ndutil.tprint("Error in %s file, wait till otacheck update it back " % configPath) 
+            ndutil.tprint("Error in %s file, the field you requested had problems... " % configPath) 
 
     return result
